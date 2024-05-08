0899436c47c694aaec8bc9357cad6dcb5568390e
jC.Souza <charlesdesouza2003@gmail.com>
2024-05-08 17:38:16 -0300

    Corrige onclick DarkMode

--- src/components/homepage/profileDisplay.js
+++ src/components/homepage/profileDisplay.js
@@ -27,15 +27,13 @@ export default function ProfileDisplay(props) {
         }}>
 
 
-        <div onClick={props.toggleDarkMode} >
-
-        <div className="button" style={{backgroundImage: props.darkMode ? `url("/img/homepage/nightfall.png")` : `url("/img/homepage/sunrise.jpg")` }}>
+        
+        <div className="button" style={{backgroundImage: props.darkMode ? `url("/img/homepage/nightfall.png")` : `url("/img/homepage/sunrise.jpg")` }} onClick={props.toggleDarkMode}>
             <button style={{marginLeft: props.darkMode ? "30px" : "0px"}} className="toggle">
                 <img className="button-icon" src={props.darkMode ? "/img/homepage/moon(1).png" : "/img/homepage/sun(1).png"} alt="darkorlight" width="20" height="20"></img>
             </button>
         </div>
-
-        </div>
+        
         </div>
         </div>
     )
