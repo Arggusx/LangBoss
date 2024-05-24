e89a5c15b5ba7a472d5672cc4615fe3d6aeb366e
Arggusx <dougclasher9@gmail.com>
2024-05-23 23:11:25 -0300

    header: Redireciona para a homepage quando clicar na logo

--- src/components/homepage/header.js
+++ src/components/homepage/header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './header.css'
 import ProfileDisplay from "./profileDisplay";
+import { Link } from "react-router-dom";
 
  const Header = (props) => {
     
@@ -12,7 +13,9 @@ import ProfileDisplay from "./profileDisplay";
         color: props.darkMode ? 'aliceblue' : 'rgb(27, 26, 26)'
     }}>
         <div className="symbol">
+            <Link to="/" style={{ textDecoration: 'none'}}>
             <img src={props.logo} className="logo-1" alt="logocrown"></img>
+            </Link>
             <h1 className="name-1">{props.name}</h1>
             <h3 className="slogan-1">{props.slogan}</h3>
         </div>
