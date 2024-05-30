48a72059f986a7a4e8cbdaa9e38943694abc085b
jC.Souza <charlesdesouza2003@gmail.com>
2024-05-30 16:22:24 -0300

    header: Remove botão perfil
    - Removido o botão perfil do header

--- src/components/homepage/profileDisplay.js
+++ src/components/homepage/profileDisplay.js
@@ -11,30 +11,11 @@ export default function ProfileDisplay(props) {
 
     return (
         <div className="profile">
-
-        <div className="profileIcon" onClick={handlelingDisplay}>
-            <img className="profileImage" src="/img/loginIcons/usuario-masculino.png" alt="profileImage"></img>
-        </div>
-
-        <div className="profileDisplay" 
-
-        style={{
-            
-            display : profileDisplay ? "block" : "none", 
-            transitionDuration: "3s",
-            
-            
-        }}>
-
-
-        
-        <div className="button" style={{backgroundImage: props.darkMode ? `url("/img/homepage/nightfall.png")` : `url("/img/homepage/sunrise.jpg")` }} onClick={props.toggleDarkMode}>
-            <button style={{marginLeft: props.darkMode ? "30px" : "0px"}} className="toggle">
-                <img className="button-icon" src={props.darkMode ? "/img/homepage/moon(1).png" : "/img/homepage/sun(1).png"} alt="darkorlight" width="20" height="20"></img>
-            </button>
-        </div>
-        
-        </div>
+            <div className="button" style={{backgroundImage: props.darkMode ? `url("/img/homepage/nightfall.png")` : `url("/img/homepage/sunrise.jpg")` }} onClick={props.toggleDarkMode}>
+                <button style={{marginLeft: props.darkMode ? "30px" : "0px"}} className="toggle">
+                    <img className="button-icon" src={props.darkMode ? "/img/homepage/moon(1).png" : "/img/homepage/sun(1).png"} alt="darkorlight" width="20" height="20"></img>
+                </button>
+            </div>
         </div>
     )
 }
