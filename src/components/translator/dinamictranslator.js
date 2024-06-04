ffc939e65d9ac7158934fe854275ff57e0ae0a9c
Esdras Tarsis <etarsiswork@gmail.com>
2024-06-04 00:43:59 -0300

    translator: Remove handleTextChange.

--- src/components/translator/dinamictranslator.js
+++ src/components/translator/dinamictranslator.js
@@ -4,34 +4,21 @@ import Country1 from "./country1";
 import Country2 from "./country2";
 import { useLocation } from 'react-router-dom';
 
-
-
 const Dinamictranslator = ({ name2, name1, short_name2, short_name1, nativeBackground, nativeTranslationBackground, isReversed, handleReverse }) => {
-
 
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const backgroundTranslation = queryParams.get("backgroundTranslation") || "";
     const background = queryParams.get("background") || "";
-
-
-
     const [text, setText] = useState("");
-
-
-    const handleChange = (event) => {
-        setText(event.target.value);
-    };
 
     const updateReceivedText = (newText) => {
         setText(newText);
     };
 
-
-
     return (
         <div className={`dinamicTranslator ${isReversed ? 'reversed' : ''}`}>
-            <Country1 handleTextChange={handleChange} background={isReversed ? background : nativeBackground} translationBackground={isReversed ? backgroundTranslation :  nativeTranslationBackground} trashEraser={"/img/translator/trash-can.png"} reverseText="/img/translator/seta-reversa.png" updateReceivedText={updateReceivedText}/>
+            <Country1 background={isReversed ? background : nativeBackground} translationBackground={isReversed ? backgroundTranslation :  nativeTranslationBackground} trashEraser={"/img/translator/trash-can.png"} reverseText="/img/translator/seta-reversa.png" updateReceivedText={updateReceivedText}/>
             <div className="languagesChoice">
                 <div className="language1"><h3>{isReversed ? name2 : name1}</h3></div>
                 <button className="reverse" onClick={handleReverse}></button>
